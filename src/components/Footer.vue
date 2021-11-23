8c87e0a97e489caeeca3afb6b64d82f57f5c1dbb
omar96developer <omakino@hotmail.it>
2021-11-23 20:44:54 +0100

    1.7

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,3 +1,56 @@
+<template>
+  <footer>
+    <section class="footer_a text-white">
+      <div class="container-lg d-flex">
+        <div class="col-6 d-flex">
+          <div class="col-6">
+            <img src="../assets/img/avada-drivers-logo-1x.png" alt="logo">
+            <p>Avada Driving School is the right choise for experianced drivers looking for a refresher course, or the brand new drive wanting to pass first time.</p>
+          </div>
+          <div class="col-6">
+            <h6>CONTACT DETAILS</h6>
+              <ul>
+                <li><i class="fas fa-home"> 12345 North main Street.NewYork. NY 555555</i></li>
+                
+                <li><i class="fas fa-phone"></i> 1.800.555.6789</li>
+                <li><i class="fas fa-envelope"></i> info@your-domain.com</li>
+                <li><i class="fas fa-link"></i> Theme-Fusion.com</li>
+              </ul>
+          </div>
+        </div>
+        <div class="col-6 d-flex">
+          <div class="col-6">
+            <h6>COURSES</h6>
+              <ul>
+                <li><i class="far fa-arrow-alt-circle-right"></i> Pass Plus</li>
+                <li><i class="far fa-arrow-alt-circle-right"></i> Intensive Course</li>
+                <li><i class="far fa-arrow-alt-circle-right"></i> Automatic</li>
+                <li><i class="far fa-arrow-alt-circle-right"></i> Instructor Training</li>
+              </ul>
+          </div>
+          <div class="col-6">
+            <img src="../assets/img/footer-ad-grid.png" alt="#">
+            <button type="button" class="btn btn-primary">MORE FROM THE BLOG</button>
+
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="footer_b">
+      <div class="container-lg">
+        <div class="row p-5 text-white">
+          <div class="col-8">
+            <p><i class="far fa-copyright"></i>Copyright 2012 - 2020: Avada Theme by ThemeFusion: All Right Reserved : Powered by WordPress</p>
+          </div>
+          <div class="col-4 text-end">
+            <span><i class="fab fa-facebook-f me-3"></i><i class="fab fa-twitter me-3"></i><i class="fab fa-youtube me-3"></i><i class="fab fa-instagram"></i></span>
+          </div>
+        </div>
+      </div>
+    </section>
+  </footer>
+</template>
+
 <script>
 
 export default {
